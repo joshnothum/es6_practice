687ff906c1c8443a6bd6c20ac0d8f936c9ab707a
Josh Nothum <joshnothum@gmail.com>
2018-02-07 21:56:02 -0600

    added some comments

--- client.js
+++ client.js
@@ -7,25 +7,27 @@ function getValue(condition) {
     const person = {
         name: 'Tom Brady'
     };
-    person.name = "Aaron Rodgers";
+    //uncomment below to make change to Rodgers
+    // person.name = "Aaron Rodgers"; 
 
     console.log(person);
     
 
-// person is a const and cannot be changed; however you can change the values of person
+// person is a const and cannot be changed; however you can change the values of person. Uncomment below to get error
 // person = {
 //     name: "Aaron Rodgers"
 // }
 
     if (condition) {
+        //uncomment below to get error
         // maxValue = 120;
         let count = 10;
-        let myValue = "Michael Vick";
-        console.log(myValue);
-        return myValue;
+        let myName = "Michael Vick";
+        console.log(myName);
+        return myName;
     } else {
 
-        console.log(myValue); 
+        console.log(myName); 
         return null;
     }
 }
@@ -34,9 +36,10 @@ function getValue(condition) {
 for (let i = 0; i < 10; i++) {
     console.log('i with let',i);
 }
+console.log(i, 'in i'); //i is undefined
 
 for (var i = 0; i < 10; i++) {
-    console.log('i with let', i);
+
 }
 console.log('i with var', i); //i is now available outside of the for loop.
 
@@ -52,7 +55,7 @@ var object = {
 //Functions in loops
 for (const key in object) {
     funcs.push(function () {
-        console.log(key);
+        console.log('this is key', key);
     });
 }
 
