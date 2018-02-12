2e05d4dadb34d7e1d73cbef64fea7725f1a5b352
Josh Nothum <joshnothum@gmail.com>
2018-02-12 00:28:57 -0600

    adding global variable notes

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 console.log('client is loaded');
+
 function getValue(condition) {
 
     var count = 40;
@@ -9,15 +10,11 @@ function getValue(condition) {
     };
     //uncomment below to make change to Rodgers
     // person.name = "Aaron Rodgers"; 
-
     console.log(person);
-    
-
-// person is a const and cannot be changed; however you can change the values of person. Uncomment below to get error
-// person = {
-//     name: "Aaron Rodgers"
-// }
-
+    // person is a const and cannot be changed; however you can change the values of person. Uncomment below to get error
+    // person = {
+    //     name: "Aaron Rodgers"
+    // }
     if (condition) {
         //uncomment below to get error
         // maxValue = 120;
@@ -27,22 +24,31 @@ function getValue(condition) {
         return myName;
     } else {
 
-        console.log(myName); 
+        console.log(myName);
         return null;
     }
 }
-
+console.log(getValue(true));
 
 for (let i = 0; i < 10; i++) {
-    console.log('i with let in loop',i);
+    console.log('i with let in loop', i);
 }
 console.log(i, 'out of loop i with let'); //i is undefined
 
 for (var i = 0; i < 10; i++) {
     console.log('i in loop with var', i);
-    
+
 }
 console.log('i out of loop with var', i); //i is now available outside of the for loop.
+
+/* const can not be used in a regular for loop.  
+
+for (const i = 0; i < 10; i++) {
+    console.log('i in loop with const', i);
+
+}
+console.log('i out of loop with const', i);
+*/
 
 var funcs = [];
 var object = {
@@ -50,19 +56,37 @@ var object = {
     b: true,
     c: true
 };
-
 //Functions in loops
+for (var key in object) {
+    funcs.push(function () {
+        console.log('key in function with var', key);
+    });
+} // returns c 3 times
+for (let key in object) {
+    funcs.push(function () {
+        console.log('key in function with let', key);
+    });
+} //returns a b c
+// you can use const in the for loop. it will work like a let.  however, the variable key will not be able to changed anywhere else.
 for (const key in object) {
-    funcs.push(function (value) {
-       return function () {
-           console.log(value);
-           
-       };
-    }(i));
-}
-
+    funcs.push(function () {
+        console.log('key in function with const', key);
+    });
+} // returns 1 2 3
 funcs.forEach(function (func) {
     func(); //outputs the number "10" ten times
 });
+//var creates a global variable. let and const do not
+var hi = 'hi';
+let hello = 'hello';
 
-console.log(getValue(true));
+const hola = 'Hola';
+
+console.log('hi with var', hi);
+console.log('window.hi with var', window.hi);
+
+console.log('hello with let', hello);
+console.log('window.hello', window.hello);
+
+console.log('hola with const', hola);
+console.log('window.hola', window.hola);
